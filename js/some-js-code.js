de00d370ada708efe5f9e74ccd22762217fb17bd
aleks-boyar <79837040+aleks-boyar@users.noreply.github.com>
2021-05-02 11:09:45 +0300

    Add files via upload

---
+++ js/some-js-code.js
@@ -0,0 +1,17 @@
+// // Task#1
+// let name = 'Джон';
+// let admin = "name";
+// alert(admin);
+//
+// // Task#2
+// let planet ='earth';
+// let currentUser = "John";
+
+// Task#3
+let str = "10";
+console.log(str, typeof str);
+let n = 10;
+console.log(n, typeof n);
+consle.log(n + str, typeof (n + str))
+
+
